000383ad7b71d39858d8ef5b7b0bf657e15db326
Jasmine Lotfy <yasmine.lotfy11.com>
2026-03-29 12:37:26 +0200

    first commit

---
+++ src/services/product.sevices.ts
@@ -0,0 +1,23 @@
+
+
+export async function getAllProducts(catId? : string) {
+
+    const base = `${process.env.BASE_URL}/products`;
+    const url = catId? `${process.env.NEXT_PUBLIC_BASE_URL}/products?category[in]=${catId}` : base ; 
+
+
+  const response = await fetch(
+    url,
+    {
+      method: "GET",
+    
+      headers: {
+        "content-type": "application/json",
+      },
+    },
+  );
+
+  const responseData = await response.json();
+
+  return responseData;
+}
